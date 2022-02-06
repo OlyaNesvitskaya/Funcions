52839b9f720f9a81bbfa80569f781f61fc974b2a
olga <nesvitskaya93@gmail.com>
2022-02-07 00:25:47 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def direction(facing, turn):
-    lst = ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW')
+    compass_points = ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW')
     if type(turn) != int or not -1080 <= turn <= 1080 or turn % 45:
-        return "Value 'turn' must be number between -1080 and should be a multiple of 45"
-    if facing not in lst:
-        return f"Select one of the directions {lst}"
+        raise TypeError("Value 'turn' must be number between -1080 and should be a multiple of 45")
+    if facing not in compass_points:
+        raise TypeError(f"Select one of the directions {compass_points}")
 
     step = turn // 45
-    start = lst.index(facing)
-    return lst[(step + start) % len(lst)]
+    start = compass_points.index(facing)
+    return compass_points[(step + start) % len(compass_points)]
 
